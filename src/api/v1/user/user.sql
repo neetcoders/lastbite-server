ebfdb19e0d583758ee7dae2375b2bcfc7691b63c
keanutaufan <keanu.taufan21@gmail.com>
2024-03-28 06:29:36 +0700

    Create register-related query

---
+++ src/api/v1/user/user.sql
@@ -0,0 +1,12 @@
+/* @name CheckUserByEmail */
+SELECT 
+    EXISTS (SELECT 1 FROM users WHERE email = :email) AS "exists";
+
+
+/* 
+    @name CreateUser
+    @param user -> (email, display_name, birth_date, user_secret)
+*/
+INSERT INTO users (email, display_name, birth_date, user_secret)
+VALUES :user
+RETURNING id, email, display_name, birth_date, created_at, updated_at;
