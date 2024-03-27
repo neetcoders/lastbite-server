7fde3973286765cec4f8db9907768c10cbc54b80
keanutaufan <keanu.taufan21@gmail.com>
2024-03-28 06:30:01 +0700

    Mount user router to v1

--- src/api/v1/v1.route.ts
+++ src/api/v1/v1.route.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+
 import { default as statusRouter } from "./status/status.route";
+import { default as userRouter } from "./user/user.route";
 
 const router = Router();
 
 router.use("/status", statusRouter);
+router.use("/users", userRouter);
 
 export default router;
