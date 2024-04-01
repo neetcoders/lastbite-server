cad5e31d915f904b491a616b7c40202498e81f44
keanutaufan <keanu.taufan21@gmail.com>
2024-04-01 21:08:21 +0700

    Define product schema and validation

---
+++ src/api/v1/product/product.schema.ts
@@ -0,0 +1,12 @@
+export type CreateProductSchema = {
+  display_name: string;
+  description?: string;
+  price_before: number;
+  price_after: number;
+  expiration_date: string;
+  stock: number;
+  category_slug: string;
+  payload: {
+    sub: string;
+  }
+}
---
+++ src/api/v1/product/product.validator.ts
@@ -0,0 +1,27 @@
+import { body } from "express-validator";
+
+export function validateCreateProduct() {
+  return [
+    body("display_name")
+      .notEmpty().withMessage("Name is required")
+      .isLength({ min: 3 }).withMessage("Name should be at least 3 characters long")
+      .isLength({ max: 64 }).withMessage("Name should be at most 64 characters long"),
+    body("description")
+      .optional()
+      .isLength({ min: 10 }).withMessage("Description should be at least 10 characters long")
+      .isLength({ max: 1250 }).withMessage("Description should be at most 1250 characters long"),
+    body("price_before")
+      .notEmpty().withMessage("Price before is required")
+      .isNumeric().withMessage("Price before must be a numeric value"),
+    body("price_after")
+      .notEmpty().withMessage("Price after is required")
+      .isNumeric().withMessage("Price after must be a numeric value"),
+    body("expiration_date")
+      .notEmpty().withMessage("Expiration date is required")
+      .isDate({ format: "YYYY-MM-DD" }).withMessage("Expiration date is not a valid date"),
+    body("category_slug")
+      .notEmpty().withMessage("Category slug is required")
+      .isLength({ min: 3 }).withMessage("Category slug should be at least 3 characters long")
+      .isLength({ max: 50 }).withMessage("Category slug should be at most 50 characters long"),
+  ];
+}
