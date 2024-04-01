c68d1e6efdab0f994ef078f708082f6fab0c0cb2
keanutaufan <keanu.taufan21@gmail.com>
2024-04-01 18:41:26 +0700

    Setup cors configuration

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { Express } from "express";
+import cors from "cors";
 
 import router from "./api/api.route";
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cors({
+  origin: ["http://localhost:3000", "https://lastbite-web-client.vercel.app"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  optionsSuccessStatus: 200,
+}));
 app.use(express.json());
 
 app.use("/", router);
