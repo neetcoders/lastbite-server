b2a67a1e344ec85a9a9e891b889a36109f0b7d43
keanutaufan <keanu.taufan21@gmail.com>
2024-04-02 06:22:39 +0700

    Get product response converter function

--- src/api/v1/product/product.controller.ts
+++ src/api/v1/product/product.controller.ts
@@ -2,7 +2,7 @@ import { ParamsDictionary } from "express-serve-static-core";
 import { Request, Response, query } from "express";
 
 import pool from "@/database/pool";
-import { CreateProductSchema, UpdateProductSchema, UpdateStockSchema } from "./product.schema";
+import { CreateProductSchema, UpdateProductSchema, UpdateStockSchema, convertToGetProductResponse } from "./product.schema";
 import { buildResponse } from "@/utils/response";
 import { getCategoryIdBySlug } from "../category/category.queries";
 import { createProduct, deleteProductById, getProductById, getProductOwnerById, updateProductDetails, updateProductStock } from "./product.queries";
@@ -40,34 +40,7 @@ export default class UserController {
       }
 
       return res.status(201).json(
-        buildResponse({
-          id: newProduct[0].id,
-          display_name: newProduct[0].display_name,
-          description: newProduct[0].description,
-          price_before: newProduct[0].price_before,
-          price_after: newProduct[0].price_after,
-          expiration_date: newProduct[0].expiration_date,
-          stock: newProduct[0].stock,
-          store: {
-            id: newProduct[0].store_id,
-            display_name: newProduct[0].store_display_name,
-            address: {
-              street: newProduct[0].address_street,
-              longitude: newProduct[0].address_longitude,
-              latitude: newProduct[0].address_latitude,
-              created_at: newProduct[0].address_created_at,
-              updated_at: newProduct[0].address_updated_at,
-            },
-            created_at: newProduct[0].store_created_at,
-            updated_at: newProduct[0].store_updated_at,
-          },
-          category: {
-            slug: newProduct[0].category_slug,
-            display_name: newProduct[0].category_display_name,
-          },
-          created_at: newProduct[0].created_at,
-          updated_at: newProduct[0].updated_at
-        }, true, "Product created successfully")
+        buildResponse(convertToGetProductResponse(newProduct[0]), true, "Product created successfully")
       );
     }
     catch (err) {
@@ -90,34 +63,7 @@ export default class UserController {
       }
 
       return res.status(200).json(
-        buildResponse({
-          id: product[0].id,
-          display_name: product[0].display_name,
-          description: product[0].description,
-          price_before: product[0].price_before,
-          price_after: product[0].price_after,
-          expiration_date: product[0].expiration_date,
-          stock: product[0].stock,
-          store: {
-            id: product[0].store_id,
-            display_name: product[0].store_display_name,
-            address: {
-              street: product[0].address_street,
-              longitude: product[0].address_longitude,
-              latitude: product[0].address_latitude,
-              created_at: product[0].address_created_at,
-              updated_at: product[0].address_updated_at,
-            },
-            created_at: product[0].store_created_at,
-            updated_at: product[0].store_updated_at,
-          },
-          category: {
-            slug: product[0].category_slug,
-            display_name: product[0].category_display_name,
-          },
-          created_at: product[0].created_at,
-          updated_at: product[0].updated_at
-        }, true, "Product fetched successfully")
+        buildResponse(convertToGetProductResponse(product[0]), true, "Product fetched successfully")
       );
     }
     catch (err) {
@@ -147,8 +93,6 @@ export default class UserController {
         );
       }
 
-      pool.query("BEGIN");
-
       const updatedProduct = await updateProductDetails.run({
         id: req.params.product_id,
         display_name: req.body.display_name,
@@ -168,42 +112,12 @@ export default class UserController {
         );
       }
 
-      pool.query("COMMIT");
-
       return res.status(200).json(
-        buildResponse({
-          id: newProduct[0].id,
-          display_name: newProduct[0].display_name,
-          description: newProduct[0].description,
-          price_before: newProduct[0].price_before,
-          price_after: newProduct[0].price_after,
-          expiration_date: newProduct[0].expiration_date,
-          stock: newProduct[0].stock,
-          store: {
-            id: newProduct[0].store_id,
-            display_name: newProduct[0].store_display_name,
-            address: {
-              street: newProduct[0].address_street,
-              longitude: newProduct[0].address_longitude,
-              latitude: newProduct[0].address_latitude,
-              created_at: newProduct[0].address_created_at,
-              updated_at: newProduct[0].address_updated_at,
-            },
-            created_at: newProduct[0].store_created_at,
-            updated_at: newProduct[0].store_updated_at,
-          },
-          category: {
-            slug: newProduct[0].category_slug,
-            display_name: newProduct[0].category_display_name,
-          },
-          created_at: newProduct[0].created_at,
-          updated_at: newProduct[0].updated_at
-        }, true, "Product updated successfully")
+        buildResponse(convertToGetProductResponse(newProduct[0]), true, "Product updated successfully")
       );
     }
     catch (err) {
       console.error(err);
-      pool.query("ROLLBACK");
       return res.status(500).json(
         buildResponse(null, false, "Internal server error")
       );
@@ -235,34 +149,7 @@ export default class UserController {
       }
 
       return res.status(200).json(
-        buildResponse({
-          id: newProduct[0].id,
-          display_name: newProduct[0].display_name,
-          description: newProduct[0].description,
-          price_before: newProduct[0].price_before,
-          price_after: newProduct[0].price_after,
-          expiration_date: newProduct[0].expiration_date,
-          stock: newProduct[0].stock,
-          store: {
-            id: newProduct[0].store_id,
-            display_name: newProduct[0].store_display_name,
-            address: {
-              street: newProduct[0].address_street,
-              longitude: newProduct[0].address_longitude,
-              latitude: newProduct[0].address_latitude,
-              created_at: newProduct[0].address_created_at,
-              updated_at: newProduct[0].address_updated_at,
-            },
-            created_at: newProduct[0].store_created_at,
-            updated_at: newProduct[0].store_updated_at,
-          },
-          category: {
-            slug: newProduct[0].category_slug,
-            display_name: newProduct[0].category_display_name,
-          },
-          created_at: newProduct[0].created_at,
-          updated_at: newProduct[0].updated_at
-        }, true, "Stock updated successfully")
+        buildResponse(convertToGetProductResponse(newProduct[0]), true, "Stock updated successfully")
       );
     }
     catch (err) {
--- src/api/v1/product/product.schema.ts
+++ src/api/v1/product/product.schema.ts
@@ -1,3 +1,5 @@
+import { IGetProductByIdResult } from "./product.queries";
+
 export type CreateProductSchema = {
   display_name: string;
   description?: string;
@@ -28,5 +30,37 @@ export type UpdateStockSchema = {
   stock: number;
   payload: {
     sub: string;
+  }
+}
+
+
+export function convertToGetProductResponse(product: IGetProductByIdResult) {
+  return {
+    id: product.id,
+    display_name: product.display_name,
+    description: product.description,
+    price_before: product.price_before,
+    price_after: product.price_after,
+    expiration_date: product.expiration_date,
+    stock: product.stock,
+    store: {
+      id: product.store_id,
+      display_name: product.store_display_name,
+      address: {
+        street: product.address_street,
+        longitude: product.address_longitude,
+        latitude: product.address_latitude,
+        created_at: product.address_created_at,
+        updated_at: product.address_updated_at,
+      },
+      created_at: product.store_created_at,
+      updated_at: product.store_updated_at,
+    },
+    category: {
+      slug: product.category_slug,
+      display_name: product.category_display_name,
+    },
+    created_at: product.created_at,
+    updated_at: product.updated_at
   }
 }
