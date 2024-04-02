b0d885c945dac35ada3766b8f6a00811a25875a0
keanutaufan <keanu.taufan21@gmail.com>
2024-04-02 08:51:21 +0700

    Create address-related sql queries

--- src/api/v1/address/address.queries.ts
+++ src/api/v1/address/address.queries.ts
@@ -39,3 +39,43 @@ const createStoreAddressIR: any = {"usedParamSet":{"address":true},"params":[{"n
 export const createStoreAddress = new PreparedQuery<ICreateStoreAddressParams,ICreateStoreAddressResult>(createStoreAddressIR);
 
 
+/** 'CreateUserAddress' parameters type */
+export interface ICreateUserAddressParams {
+  address: {
+    street: string | null | void,
+    longitude: number | null | void,
+    latitude: number | null | void,
+    user_id: string | null | void
+  };
+}
+
+/** 'CreateUserAddress' return type */
+export interface ICreateUserAddressResult {
+  created_at: Date;
+  id: string;
+  latitude: number;
+  longitude: number;
+  street: string;
+  updated_at: Date;
+  user_id: string | null;
+}
+
+/** 'CreateUserAddress' query type */
+export interface ICreateUserAddressQuery {
+  params: ICreateUserAddressParams;
+  result: ICreateUserAddressResult;
+}
+
+const createUserAddressIR: any = {"usedParamSet":{"address":true},"params":[{"name":"address","required":false,"transform":{"type":"pick_tuple","keys":[{"name":"street","required":false},{"name":"longitude","required":false},{"name":"latitude","required":false},{"name":"user_id","required":false}]},"locs":[{"a":66,"b":73}]}],"statement":"INSERT INTO address (street, longitude, latitude, user_id)\nVALUES :address\nRETURNING id, street, longitude, latitude, user_id, created_at, updated_at"};
+
+/**
+ * Query generated from SQL:
+ * ```
+ * INSERT INTO address (street, longitude, latitude, user_id)
+ * VALUES :address
+ * RETURNING id, street, longitude, latitude, user_id, created_at, updated_at
+ * ```
+ */
+export const createUserAddress = new PreparedQuery<ICreateUserAddressParams,ICreateUserAddressResult>(createUserAddressIR);
+
+
--- src/api/v1/address/address.sql
+++ src/api/v1/address/address.sql
@@ -5,3 +5,12 @@
 INSERT INTO address (street, longitude, latitude)
 VALUES :address
 RETURNING id, street, longitude, latitude, created_at, updated_at;
+
+
+/* 
+    @name CreateUserAddress
+    @param address -> (street, longitude, latitude, user_id)
+*/
+INSERT INTO address (street, longitude, latitude, user_id)
+VALUES :address
+RETURNING id, street, longitude, latitude, user_id, created_at, updated_at;
