ae0cfc7a6301a06dc2bbf41bedf22908025b1158
keanutaufan <keanu.taufan21@gmail.com>
2024-04-05 06:44:52 +0700

    Create order and order_product sql model

---
+++ src/database/migrations/8_create_orders.sql
@@ -0,0 +1,30 @@
+-- Up Migration
+
+CREATE TYPE ORDER_STATUS AS ENUM ('in-cart', 'waiting', 'processed', 'ready', 'done', 'cancelled', 'rejected');
+
+CREATE TABLE orders (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    customer_id UUID NOT NULL,
+    store_id UUID NOT NULL,
+
+    status ORDER_STATUS NOT NULL,
+
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP NOT NULL DEFAULT now(),
+
+    CONSTRAINT fk_orders_users FOREIGN KEY (customer_id) REFERENCES users (id),
+    CONSTRAINT fk_orders_store FOREIGN KEY (store_id) REFERENCES store (id)
+);
+
+CREATE INDEX idx_fk_orders_users ON orders (customer_id);
+CREATE INDEX idx_fk_orders_store ON orders (store_id);
+
+CREATE TRIGGER trigger_update_timestamp
+AFTER UPDATE ON orders
+FOR EACH ROW
+EXECUTE PROCEDURE update_timestamp();
+
+-- Down Migration
+
+DROP TABLE orders;
