3412b3bbe0ba791f57cc96951c10fd81e5ed8009
keanutaufan <keanu.taufan21@gmail.com>
2024-04-08 08:03:28 +0700

    Refine image upload filter

--- src/services/multer.service.ts
+++ src/services/multer.service.ts
@@ -1,6 +1,9 @@
 import { buildResponse } from "@/utils/response";
 import { Request, Response, NextFunction } from "express";
 import multer from "multer";
+import path from "path";
+
+const ALLOWED_IMAGE_EXT = [".png", ".jpg", ".jpeg", ".webp"];
 
 export const upload = multer({
   storage: multer.memoryStorage(),
@@ -16,9 +19,10 @@ export async function validateImageUpload(req: Request, res: Response, next: Nex
     );
   }
 
-  if (req.file.mimetype.indexOf("image") < 0) {
+  const ext = path.extname(req.file.originalname);
+  if (!ALLOWED_IMAGE_EXT.includes(ext)) {
     return res.status(422).json(
-      buildResponse(null, false, "Only image is allowed")
+      buildResponse(null, false, `Only ${ALLOWED_IMAGE_EXT.join(", ")} files are supported`)
     );
   }
 
